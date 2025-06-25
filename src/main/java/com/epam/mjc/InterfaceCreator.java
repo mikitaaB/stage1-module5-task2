2237023aeab606a84244cb2f4bb4a885bc86981d
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-25 04:27:58 +0300

    Implement divideBy method in InterfaceCreator to perform division on a list of integers

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            List<Integer> result = new ArrayList<>();
+            for (Integer num : list) {
+                result.add(num / divider);
+            }
+            return result;
+        };
     }
 }
